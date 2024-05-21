27accb15a11852f28e82134db0b63ac794ec3ef7
aebinessa <abinessa0004@sdsu.edu>
2024-05-21 09:47:28 +0300

    ILXL-3 <finished the page>

--- app/src/main/java/com/binjesus/kfhr_mobile/composables/WelcomeScreen.kt
+++ app/src/main/java/com/binjesus/kfhr_mobile/composables/WelcomeScreen.kt
@@ -72,7 +72,7 @@ fun LoginScreen() {
 
                 Text(
                     text = "Welcome!",
-                    fontSize = 24.sp,
+                    fontSize = 26.sp,
                     fontWeight = FontWeight.Bold,
                     color = Color.White,
                     modifier = Modifier.padding(bottom = 8.dp).align(Alignment.Start),

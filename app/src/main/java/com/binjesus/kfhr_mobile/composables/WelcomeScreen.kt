af8c8007c438b61f550d13e3ba27375034876544
aebinessa <abinessa0004@sdsu.edu>
2024-05-21 09:36:23 +0300

    welcome page frontend done

---
+++ app/src/main/java/com/binjesus/kfhr_mobile/composables/WelcomeScreen.kt
@@ -0,0 +1,117 @@
+package com.binjesus.kfhr_mobile.composables
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.binjesus.kfhr_mobile.R
+
+@Composable
+fun LoginScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(0.dp)
+            .background(Color.White),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.height(48.dp))
+
+        Text(
+            text = "KFHR",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.kfh_logo),
+            contentDescription = "KFHR Logo",
+            modifier = Modifier
+                .size(220.dp)
+                .padding(bottom = 16.dp),
+            contentScale = ContentScale.FillBounds
+        )
+        Spacer(modifier = Modifier.height(48.dp)) // Adjust the height to move the content down
+
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .background(Color(0xFF078544), RoundedCornerShape(topStart = 10.dp, topEnd = 10.dp))
+                .padding(16.dp)
+        ) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Spacer(modifier = Modifier.height(48.dp))
+
+                Text(
+                    text = "Welcome!",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White,
+                    modifier = Modifier.padding(bottom = 8.dp).align(Alignment.Start),
+                )
+                Spacer(modifier = Modifier.height(22.dp))
+
+                Text(
+                    text = "please sign in with your Employee ID and Password!",
+                    fontSize = 18.sp,
+                    color = Color.White,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(bottom = 16.dp).fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(18.dp))
+                Button(
+                    onClick = { /* TODO: Add sign-in logic */ },
+                    modifier = Modifier.fillMaxWidth().padding(horizontal = 40.dp).height(56.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.White),
+                    shape = RoundedCornerShape(14.dp)
+                ) {
+                    Text(
+                        text = "Sign In",
+                        color = Color(0xFF4CAF50),
+                        fontSize = 20.sp
+
+                    )
+
+                }
+                Spacer(modifier = Modifier.height(20.dp))
+                Text(
+                    text = "dont have an account? Contact IT",
+                    fontSize = 18.sp,
+                    color = Color.White,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .padding(top = 8.dp)
+                        .clickable { /* No functionality */ }
+                )
+            }
+        }
+    }
+}

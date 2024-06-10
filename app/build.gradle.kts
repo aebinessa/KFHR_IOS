8b51726e992b0d7ee5d15cb8133af2fec7cd193a
aebinessa <abinessa0004@sdsu.edu>
2024-06-10 14:11:15 +0300

    modifies build gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.binjesus.kfhr_mobile"
-        minSdk = 33
+        minSdk = 30
         targetSdk = 34
         versionCode = 1
         versionName = "1.1"

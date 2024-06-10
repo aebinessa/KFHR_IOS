8205f401fdff89dbdf4491c7f4d53c78b4147c51
aebinessa <abinessa0004@sdsu.edu>
2024-06-10 10:48:55 +0300

    modifies home page UI

--- app/src/main/java/com/binjesus/kfhr_mobile/composables/Home.kt
+++ app/src/main/java/com/binjesus/kfhr_mobile/composables/Home.kt
@@ -1,19 +1,19 @@
 import android.annotation.SuppressLint
 import android.util.Log
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -30,13 +30,14 @@ import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.concurrent.timer
 
+val DarkGreen = Color(0xFF006400)
+
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter", "SuspiciousIndentation")
 @Composable
 fun HomeScreen(
     navController: NavHostController,
     viewModel: KFHRViewModel = androidx.lifecycle.viewmodel.compose.viewModel()
 ) {
-
     var isCheckedOut = viewModel.attendance?.checkOutTime != null
     var timeLeft by remember { mutableStateOf(calculateTimeLeft(isCheckedOut)) }
     timer(initialDelay = 1000L, period = 1000L) {
@@ -45,107 +46,43 @@ fun HomeScreen(
     Box(
         modifier = Modifier
             .fillMaxSize()
-
     ) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(16.dp),
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.CenterHorizontally
+                .padding(16.dp)
         ) {
-            Spacer(modifier = Modifier.height(12.dp))
-
             HomeTopBar(viewModel = viewModel, navController = navController)
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            Card(
-                shape = RoundedCornerShape(8.dp),
-                elevation = 4.dp,
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Column(
-                    modifier = Modifier.padding(16.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    var timeLeftTitle = "Please check in to see time left"
-                    if (viewModel.attendance?.checkInTime != null) {
-                        timeLeftTitle = "Time left till check out"
-                    }
-
-                    Text(
-                        text = timeLeft,
-                        fontSize = 36.sp,
-                        fontWeight = FontWeight.Bold
-                    )
-                    Text(
-                        text = timeLeftTitle,
-                        fontSize = 16.sp,
-                        color = Color.Gray
-                    )
-                }
-            }
 
             Spacer(modifier = Modifier.height(16.dp))
 
             Column(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp),
-                verticalArrangement = Arrangement.spacedBy(16.dp)
+                    .fillMaxSize()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                var checkInTime = "--:--"
-                var checkOutTime = "--:--"
-                if (viewModel.attendance != null) {
-                    val timeFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
-                    Log.e("SSSSS", viewModel.attendance!!.checkInTime.toString())
-                    Log.e("SSSSS", viewModel.attendance!!.checkOutTime.toString())
-                    if (viewModel.attendance!!.checkInTime != null)
-                        checkInTime =
-                            timeFormat.format(viewModel.attendance!!.checkInDateTimeObject())
-                    if (viewModel.attendance!!.checkOutTime != null)
-                        checkOutTime =
-                            timeFormat.format(viewModel.attendance!!.checkOutDateTimeObject())
-
-                }
-
-                CheckInOutCard(
-                    time = checkInTime,
-                    label = "Check In"
-                )
-
-                CheckInOutCard(
-                    time = checkOutTime,
-                    label = "Check Out"
-                )
-
-            }
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            Card(
-                shape = RoundedCornerShape(8.dp),
-                elevation = 4.dp,
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Column(
-                    modifier = Modifier.padding(16.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth() // Adjusted to be wider
+                        .border(2.dp, DarkGreen, RoundedCornerShape(8.dp))
                 ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.clock), // Replace with actual image resource
-                        contentDescription = "Clock",
-                        modifier = Modifier.size(60.dp) // Smaller size
-                    )
-                    Text(
-                        text = "Late Minutes Left : ${viewModel.lateMinutesLeft?.minutesLeft}",
-                        fontSize = 16.sp,
-                        color = Color.Gray
-                    )
+                    CombinedCheckInOutCard(viewModel = viewModel, timeLeft = timeLeft)
                 }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .border(2.dp, DarkGreen, RoundedCornerShape(8.dp))
+                ) {
+                    LateMinutesCard(viewModel = viewModel)
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
             }
-            Spacer(modifier = Modifier.height(16.dp))
         }
 
         NFCFABButton(
@@ -166,14 +103,22 @@ fun HomeTopBar(viewModel: KFHRViewModel, navController: NavHostController) {
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
-        Image(
-            painter = rememberImagePainter(data = viewModel.employee?.employeePic),
-            contentDescription = "Profile Picture",
+        Box(
             modifier = Modifier
-                .size(70.dp)
-                .background(Color.Gray, shape = CircleShape),
-            contentScale = ContentScale.Crop
-        )
+                .size(60.dp) // Total size with border
+                .background(DarkGreen, shape = CircleShape) // Black border
+                .padding(3.dp) // Adjust the padding to control border thickness
+        ) {
+            Image(
+                painter = rememberImagePainter(data = viewModel.employee?.employeePic),
+                contentDescription = "Profile Picture",
+                modifier = Modifier
+                    .size(54.dp) // Size of the image inside the border
+                    .background(Color.Gray, shape = CircleShape)
+                    .clip(CircleShape), // Ensure the image is clipped to a circle shape
+                contentScale = ContentScale.Crop
+            )
+        }
         Spacer(modifier = Modifier.width(12.dp))
         var name = ""
         if (viewModel.employee?.employeeName != null) {
@@ -199,13 +144,114 @@ fun HomeTopBar(viewModel: KFHRViewModel, navController: NavHostController) {
 }
 
 @Composable
-fun CheckInOutCard(time: String, label: String) {
+fun CombinedCheckInOutCard(viewModel: KFHRViewModel, timeLeft: String) {
     Card(
         shape = RoundedCornerShape(8.dp),
         elevation = 4.dp,
-        modifier = Modifier
-            .padding(horizontal = 16.dp, vertical = 8.dp)
-            .fillMaxWidth()
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            var timeLeftTitle = "Please check in to see time left"
+            if (viewModel.attendance?.checkInTime != null) {
+                timeLeftTitle = "Time left till check out"
+            }
+
+            Text(
+                text = timeLeftTitle,
+                fontSize = 16.sp,
+                color = Color.Gray
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = timeLeft,
+                fontSize = 36.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                var checkInTime = "--:--"
+                var checkOutTime = "--:--"
+                if (viewModel.attendance != null) {
+                    val timeFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
+                    Log.e("SSSSS", viewModel.attendance!!.checkInTime.toString())
+                    Log.e("SSSSS", viewModel.attendance!!.checkOutTime.toString())
+                    if (viewModel.attendance!!.checkInTime != null)
+                        checkInTime =
+                            timeFormat.format(viewModel.attendance!!.checkInDateTimeObject())
+                    if (viewModel.attendance!!.checkOutTime != null)
+                        checkOutTime =
+                            timeFormat.format(viewModel.attendance!!.checkOutDateTimeObject())
+                }
+
+                // Using weight modifier correctly inside Row scope
+                CheckInOutCard(
+                    time = checkInTime,
+                    label = "Check In",
+                    modifier = Modifier.weight(1f)
+                )
+
+                CheckInOutCard(
+                    time = checkOutTime,
+                    label = "Check Out",
+                    modifier = Modifier.weight(1f)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun LateMinutesCard(viewModel: KFHRViewModel) {
+    val progress = viewModel.lateMinutesLeft?.minutesLeft?.toFloat()?.div(60) ?: 0f
+
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        elevation = 4.dp,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Canvas(modifier = Modifier.size(60.dp)) {
+                drawArc(
+                    color = Color.Red,
+                    startAngle = -90f,
+                    sweepAngle = progress * 360,
+                    useCenter = true
+                )
+                drawArc(
+                    color = Color.Gray,
+                    startAngle = -90f + progress * 360,
+                    sweepAngle = (1 - progress) * 360,
+                    useCenter = true,
+                    style = Stroke(width = 8.dp.toPx())
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = "Late Minutes Left : ${viewModel.lateMinutesLeft?.minutesLeft ?: "--"}",
+                fontSize = 16.sp,
+                color = Color.Gray
+            )
+        }
+    }
+}
+
+@Composable
+fun CheckInOutCard(time: String, label: String, modifier: Modifier = Modifier) {
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        elevation = 4.dp,
+        modifier = modifier
+            .padding(horizontal = 8.dp, vertical = 8.dp)
     ) {
         Column(
             modifier = Modifier
@@ -225,7 +271,6 @@ fun CheckInOutCard(time: String, label: String) {
         }
     }
 }
-
 
 fun calculateTimeLeft(isCheckedOut: Boolean): String {
     val currentTime = Date()
